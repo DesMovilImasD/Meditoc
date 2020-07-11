82e89509dad6c0e5ff91f5387ff16894f0997d22
cristopherNG <g098@live.com.mx>
2020-07-11 15:49:09 -0500

    Se agrega servicio para descargar reporte

--- IMD.Meditoc.Pagos/IMD.Meditoc.Pagos.Web/Controllers/ReportesController.cs
+++ IMD.Meditoc.Pagos/IMD.Meditoc.Pagos.Web/Controllers/ReportesController.cs
@@ -9,6 +9,8 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using IMD.Meditoc.Pagos.Business;
+using System.IO;
+using System.Net.Http.Headers;
 
 namespace IMD.Meditoc.Pagos.Web.Controllers
 {
@@ -39,6 +41,42 @@ namespace IMD.Meditoc.Pagos.Web.Controllers
             }
             return response;
         }
+
+        [HttpGet]
+        [Route("Api/Meditoc/Pagos/Descargar/Reporte/Ordenes")]
+        public HttpResponseMessage CObtenerDocumento([FromUri]string psStatus = null, [FromUri]string psType = null, [FromUri]DateTime? pdtFechaInicio = null, [FromUri]DateTime? pdtFechaFinal = null)
+        {
+            HttpResponseMessage response;
+
+            string metodo = nameof(this.CObtenerDocumento);
+            logger.Info(IMDSerialize.Serialize(67823458078621, $"Inicia {metodo}"));
+
+            try
+            {
+                BusReportes busReportes = new BusReportes();
+                IMDResponse<MemoryStream> resExcel = busReportes.BDescargarReporte(psStatus, psType, pdtFechaInicio, pdtFechaFinal);
+                if (resExcel.Code != 0)
+                {
+                    response = Request.CreateResponse(HttpStatusCode.InternalServerError, resExcel.Message);
+                }
+                else
+                {
+                    response = Request.CreateResponse(HttpStatusCode.OK);
+                    response.Content = new StreamContent(resExcel.Result);
+                    response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
+                    response.Content.Headers.ContentDisposition.FileName = "ReporteOrdenesConekta.xlsx";
+                    response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+                }
+            }
+            catch (Exception ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.InternalServerError, "No se pudo consultar el documento del contratista");
+
+                logger.Error(IMDSerialize.Serialize(67823458079398, $"Error en {metodo}: {ex.Message}", ex, response));
+            }
+            return response;
+        }
+
         [HttpGet]
         [Route("Api/Meditoc/Check/Server/Status")]
         public IMDResponse<bool> CCheckServer()
