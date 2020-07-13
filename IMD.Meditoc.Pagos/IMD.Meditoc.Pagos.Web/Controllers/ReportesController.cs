44dececffe98f9d79cfed544bdff7a1f24af1fa9
cristopherNG <g098@live.com.mx>
2020-07-13 11:06:33 -0500

    Metodos de conekta pagos

--- IMD.Meditoc.Pagos/IMD.Meditoc.Pagos.Web/Controllers/ReportesController.cs
+++ IMD.Meditoc.Pagos/IMD.Meditoc.Pagos.Web/Controllers/ReportesController.cs
@@ -11,13 +11,15 @@ using System.Web.Http;
 using IMD.Meditoc.Pagos.Business;
 using System.IO;
 using System.Net.Http.Headers;
+using System.Web.Http.Cors;
 
 namespace IMD.Meditoc.Pagos.Web.Controllers
 {
+    [EnableCors("*", "*", "*")]
     public class ReportesController : ApiController
     {
         private static readonly ILog logger = LogManager.GetLogger(typeof(ReportesController));
-
+        [DisableCors]
         [HttpGet]
         [Route("Api/Meditoc/Pagos/Reporte/Ordenes")]
         public IMDResponse<List<EntOrderReporte>> CObtenerReporteOrdenes([FromUri]string psStatus = null, [FromUri]string psType = null, [FromUri]DateTime? pdtFechaInicio = null, [FromUri]DateTime? pdtFechaFinal = null)
