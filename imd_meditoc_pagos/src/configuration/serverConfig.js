1f24d4f96192586f24f577da59b660884b3e4d38
cristopherNG <g098@live.com.mx>
2020-08-23 16:36:33 -0500

    Se agregan configuraciones de produccion

--- imd_meditoc_pagos/src/configuration/serverConfig.js
+++ imd_meditoc_pagos/src/configuration/serverConfig.js
@@ -9,7 +9,11 @@
 // const serverWs = 'http://localhost:9000/'
 
 //locutorios
-const serverWa = 'http://148.240.238.149/wa/'
-const serverWs = 'http://148.240.238.149/ws/'
+ const serverWa = 'http://148.240.238.149/wa/'
+ const serverWs = 'http://148.240.238.149/ws/'
+
+//Producción
+//const serverWa = 'https://201.46.92.51/WA/'
+//const serverWs = 'http://201.46.92.51/'
 
 export { serverWa, serverWs }
