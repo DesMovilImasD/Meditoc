d7e8cde138ed79bcf299eaecaef4d1cdf8728411
José Martínez <jmartinez@imasd.com.mx>
2020-08-28 12:04:39 -0500

    Se agrega el nombre del tipo cuenta y perfil

--- IMD.Meditoc.CallCenter.Mx/IMD.Meditoc.CallCenter.Mx.Entities/CGU/EntUsuario.cs
+++ IMD.Meditoc.CallCenter.Mx/IMD.Meditoc.CallCenter.Mx.Entities/CGU/EntUsuario.cs
@@ -10,7 +10,9 @@ namespace IMD.Meditoc.CallCenter.Mx.Entities.CGU
     {
         public int iIdUsuario { get; set; }
         public int iIdTipoCuenta { get; set; }
+        public string sTipoCuenta { get; set; }
         public int iIdPerfil { get; set; }
+        public string sPerfil { get; set; }
         public string sUsuario { get; set; }
         public string sPassword { get; set; }
         public string sNombres { get; set; }
